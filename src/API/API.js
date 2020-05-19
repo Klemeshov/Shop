6e6d78807494861fb9716a113c83b3c2ebc9e42e
Dmitry Klemeshov <dima-0510@mail.ru>
2020-05-19 18:12:23 +0300

    ajax req from react test

--- src/API/API.js
+++ src/API/API.js
@@ -1,15 +1,33 @@
-// import * as moysklad from "moysklad";
+import * as moysklad from "moysklad";
 // import fetch from 'node-fetch';
-import * as axios from "axios"
-import * as ip from "ip"
+// import * as axios from "axios"
+//
+// const ms = axios.create({
+//     baseURL: 'http://127.0.0.1:5000/'
+// });
+//
+//
+// export const ProductsAPI = {
+//     getProducts(limit, offset){
+//         return ms.get(`/entity/product`, {params: {limit, offset}}).then(res=>res.data)
+//     }
+// };
 
-const ms = axios.create({
-    baseURL: 'http://'+ip.address()+':5000/'
+
+const login = "admin@dima-0510";
+const password = "794cdc36689b";
+const ms = moysklad({
+    login,
+    password
 });
-
 
 export const ProductsAPI = {
     getProducts(limit, offset){
-        return ms.get(`/entity/product`, {params: {limit, offset}}).then(res=>res.data)
+        return ms.GET("entity/product", {limit, offset}).then(require=>{
+            const answer = {};
+            answer.products = require.rows;
+            answer.size = require.meta.size;
+            return answer
+        });
     }
 };
