b307ed7233518d06a100d019164b7d3644e9aa63
Klemeshov <dima-0510@mail.ru>
2021-02-02 22:34:53 +0500

    Update project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./Components/Header/Header";
 import Preloader from "./Components/common/Preloader/Preloader";
-
 const MainContent = React.lazy(() => import("./Components/MainContent/MainContent"));
 
 class App extends React.Component {
